0e2ebe72456eecceeff9321b869d38230080b2a2
Forhad5194 <forhadhossain5194@gmail.com>
2024-10-29 16:02:21 +0600

    Get form data succesfuly on console

--- src/app/(WithCommonLayout)/(user)/profile/create-post/page.tsx
+++ src/app/(WithCommonLayout)/(user)/profile/create-post/page.tsx
@@ -18,6 +18,8 @@ import FXSelect from "@/src/components/form/FXSelect";
 import { allDivision } from "@bangladeshi/bangladesh-address"
 import { useGetCategory } from "@/src/hooks/category";
 import { useState } from "react";
+import FXTextarea from "@/src/components/form/FXTextArea";
+import { useUser } from "@/src/context/user.provider";
 
 
 const cityOptions = allDivision().sort().map((city: string) => ({
@@ -75,11 +77,20 @@ export default function page() {
     name: "questions",
   });
 
+  const {user} = useUser()
+
+
+
+
+
+
+
   const onSubmit: SubmitHandler<FieldValues> = (data) => {
     const postData = {
       ...data,
       questions: data.questions.map((que: { value: string }) => que.value),
-      dateFound: dateToISO(data.dateFound)
+      dateFound: dateToISO(data.dateFound),
+      user : user!._id
 
     };
 
@@ -147,7 +158,7 @@ export default function page() {
                 <FXSelect name="category" disabled={!categorySuccess} label="category" options={categoryOption} />
               </div>
               <div className="min-w-fit flex-1">
-                <label  className="flex h-14 w-full cursor-pointer items-center justify-center rounded-xl border-2 border-default-200 text-default-500 shadow-sm transition-all duration-100 hover:border-default-400" htmlFor="image">
+                <label className="flex h-14 w-full cursor-pointer items-center justify-center rounded-xl border-2 border-default-200 text-default-500 shadow-sm transition-all duration-100 hover:border-default-400" htmlFor="image">
                   Upload Image
                   <input onChange={(e) => handleImageChange(e)} className="hidden" multiple type="file" id="image" />
                 </label>
@@ -158,10 +169,10 @@ export default function page() {
 
               imagesPreviwes.length > 0 && imagesPreviwes.map((imageDataUrl) => (
                 <div
-                key={imageDataUrl}
-                className="relative h-48 w-48 rounded-xl border-2 border-dashed border-default-300 p-2 "
-                
-                
+                  key={imageDataUrl}
+                  className="relative h-48 w-48 rounded-xl border-2 border-dashed border-default-300 p-2 "
+
+
                 >
                   <img className="h-full w-full object-cover object-center rounded-md " src={imageDataUrl} />
                 </div>
@@ -171,9 +182,13 @@ export default function page() {
             }
             </div>
 
-            
 
 
+            <div className="flex flex-wrap-reverse gap-2 py-2">
+              <div className="min-w-fit flex-1">
+                <FXTextarea label="Description" name="description" />
+              </div>
+            </div>
 
 
 
@@ -184,13 +199,26 @@ export default function page() {
 
             <div className="flex justify-between items-center">
               <h1 className="text-xl">Owner verification questions</h1>
-              <Button onClick={() => handleFieldAppend()}>Append</Button>
+              <Button onClick={() => handleFieldAppend()}>
+
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+                  <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
+                </svg>
+
+
+
+              </Button>
             </div>
 
             {fields.map((field, index) => (
-              <div key={field.id} className="flex items-center">
+              <div key={field.id} className="flex items-center gap-5 ">
                 <FXinput name={`questions.${index}.value`} label="Question" />
-                <Button onClick={() => remove(index)}>Remove</Button>
+                <div className="my-8" onClick={() => remove(index)}>
+                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className=" cursor-pointer text-red-500  size-6">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
+                  </svg>
+
+                </div>
               </div>
             ))}
 
